46ac67e61200803059ff5bdf5d1eb6821e9a7166
kief <kmorris@thoughtworks.com>
2018-07-06 18:11:29 +0100

    Added a custom inspec resource to expose more info about iam role

--- account/aws-roles/tests/inspec/iam/controls/spin_account_manager_role.rb
+++ account/aws-roles/tests/inspec/iam/controls/spin_account_manager_role.rb
@@ -4,6 +4,13 @@ title 'Spin Account Manager IAM role'
 
 component = attribute('component', description: 'Which component things should be tagged')
 
-describe aws_iam_role("spin_account_manager-#{component}") do
+describe aws_iam_role_extended("spin_account_manager-#{component}") do
   it { should exist }
 end
+
+# policy_struct = aws_iam_policy("spin_stack_manager-#{component}").policy
+
+# describe "spin_stack_manager-#{component} policy" do
+#   subject { policy_struct['Principle'].first['Condition'] }
+#   it { should include 'NotIpAddress' }
+# end
--- account/aws-roles/tests/inspec/iam/controls/spin_stack_manager_role.rb
+++ account/aws-roles/tests/inspec/iam/controls/spin_stack_manager_role.rb
@@ -4,6 +4,6 @@ title 'Spin Stack Manager IAM role'
 
 component = attribute('component', description: 'Which component things should be tagged')
 
-describe aws_iam_role("spin_stack_manager-#{component}") do
+describe aws_iam_role_extended("spin_stack_manager-#{component}") do
   it { should exist }
 end
